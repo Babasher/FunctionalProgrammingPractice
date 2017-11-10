f2a94a1b7945b6a2ac1c5667dd10b1b0ddb089ef
Omar <omoham5@gmail.com>
2017-11-09 20:05:14 -0600

    Implementing Collection and Function

--- src/Functions/Operations.java
+++ src/Functions/Operations.java
@@ -1,14 +1,18 @@
 package Functions;
 
-import java.util.function.BiFunction;
 
-public class Operations{
+
+import java.util.Collection;
+import java.util.function.*;
+
+
+public class Operations<T>{
 	
 	BiFunction<Double, Double, Double> addFunction = (numb1, numb2) -> numb1 + numb2;
-	BiFunction<Double,Double,Double> multFunction = (numb1, numb2) -> numb1 * numb2;
-	BiFunction<Double,Double,Double> subFunction = (numb1, numb2) -> numb1 - numb2;
-	BiFunction<Double,Double,Double> divFunction = (numb1, numb2) -> numb1 / numb2; 
-
+	BiFunction<Double, Double, Double> multFunction = (numb1, numb2) -> numb1 * numb2;
+	BiFunction<Double, Double, Double> subFunction = (numb1, numb2) -> numb1 - numb2;
+	BiFunction<Double, Double, Double> divFunction = (numb1, numb2) -> numb1 / numb2; 
+	
 	public static void main(String args[]) {
 		Operations operate = new Operations();
 		operate.run();
@@ -20,17 +24,20 @@ public class Operations{
 		System.out.println(Operate(subFunction,10,5));
 		System.out.println(Operate(divFunction,100,2));
 		
-		
-		
-	}// gud luk
+		//System.out.println( map(x -> x*2, ); 
+		//Figuring out how to pass in a collection! 
+		//ImmutableList.of does not work here
 	
+	}
 	
+	Collection<T> map(Function<T,T> Operate, Collection<T> collection) {
+		collection.stream()
+			.map(Operate);
+		return collection;
+	}
 	
 	public double Operate(BiFunction<Double, Double, Double > operation, double numb1, double numb2) {
 		return operation.apply(numb1, numb2);
 	}
 
 }
-
-
-
